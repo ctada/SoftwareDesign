907ec3f5f87e8b51f5c80abcd8ba3573a71a605e
ctada <celine.ta@students.olin.edu>
2015-03-13 13:55:44 -0400

    functional word count analysis

--- toolbox/word_frequency_analysis/frequency.py
+++ toolbox/word_frequency_analysis/frequency.py
@@ -9,7 +9,19 @@ def get_word_list(file_name):
 		returns a list of the words used in the book as a list.
 		All words are converted to lower case.
 	"""
-	pass
+	# strip away header
+	f=open(file_name, 'r')
+	words=[]
+	start = False
+	for line in f: 
+		if "*** START" in line:
+			start = True
+		elif start: #moves to line after ***START trigger
+			line = string.lower(line) #making everything lowercase
+			for word in line:
+				word= string.strip(word, string.punctuation) #won't strip punctuation in the middle of the word, i.e. "don't"
+			words.extend(line.split()) #using split without an argument removes all whitespace when splitting into words
+	return words
 
 def get_top_n_words(word_list, n):
 	""" Takes a list of words as input and returns a list of the n most frequently
@@ -21,4 +33,17 @@ def get_top_n_words(word_list, n):
 		returns: a list of n most frequently occurring words ordered from most
 				 frequently to least frequentlyoccurring
 	"""
-	pass
+	word_dict = {}
+
+	# counts number of repeats for each word
+	for word in word_list: 
+		if word in word_dict:
+			word_dict[word]+=1
+		else:
+			word_dict[word] = 1
+	top_list = sorted(word_dict, key=word_dict.get, reverse=True)
+	return top_list[0:n]
+
+if __name__ == "__main__":
+	l= get_word_list('grimm.txt')
+	print get_top_n_words(l, 100)
